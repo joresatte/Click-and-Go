d1acd81d7b99f484b7a949fd6f7e3ddd6a3ba5ec
joresatte <joresatte@yahoo.fr>
2023-11-10 18:00:00 +0100

    Pull data from external API methods

--- back/src/webserver.py
+++ back/src/webserver.py
@@ -1,7 +1,9 @@
+from email import header
 from flask import Flask, request
 from flask_cors import CORS
 from src.lib.utils import object_to_json
-
+import requests
+import json
 
 def create_app(repositories):
     app = Flask(__name__)
@@ -31,5 +33,25 @@ def create_app(repositories):
         remove_one_customer = repositories['customer_data'].deleted_record_by_id(id)
         return ""
     
+    def pull_data_from_external_Api():
+        url= 'https://api.covid19india.org/state_district_wise.json'
+        header= {"Content-Type":"appliction/json"}
+        response_API = requests.get(url, header)
+        print(response_API.status_code)
+        data = response_API.json #response_API.text
+        print(data)
+        parse_json= json.loads(data)
+        active_case = parse_json['id']['cliente']['dni']['address']['phone']['delivery_note']['order_data']['orders_packages']['receptor_data']['returned_product']
+        print("Active cases in South Andaman:", active_case)
+        return active_case
+    
+    # def save_data_from_external_Api():
+    #     drivers= pull_data_from_external_Api()
+    #     for dr in drivers:
+    #         name = dr["familyName"]
+    #         number = dr["permanentNumber"]
+    #         sql = 'INSERT INTO Drivers (name,number) VALUES(?,?)'
+    #         val = (name,number)
+    #         cur.execute(sql,val)
 
     return app
