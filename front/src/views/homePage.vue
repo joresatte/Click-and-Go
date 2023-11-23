777ac2a7fadc2522bb7593a5a739514f5a2220db
joresatte <joresatte@yahoo.fr>
2023-11-23 13:34:58 +0100

    fixed bugs

--- front/src/views/homePage.vue
+++ front/src/views/homePage.vue
@@ -4,81 +4,82 @@
   </div>
   <div class="card" v-if="showDataView">
       <DataView :value="customers" :layout="layout" :sortOrder="sortOrder" :sortField="sortField">
-          <template #header>
-              <div class="overflow-hidden">
-                  <div class="flex">
-                      <Dropdown v-model="sortKey" 
+            <template #header>
+                <div class="overflow-hidden">
+                    <div class="flex">
+                        <Dropdown v-model="sortKey" 
                           class="flex justify-content-start font-bold border-round"
                           :options="sortOptions" optionLabel="label" placeholder="Filtrar" @change="onSortChange($event)" />
-                          <span class="ml-1 p-input-icon-left ">
-                              <i class="pi pi-search " style="color: blue;"/>
-                              <InputText 
-                              type="text" v-model="filteredText" 
-                              placeholder="Buscar" 
-                              style="width: 8em; color: blue;"/>
-                          </span>
-                      <DataViewLayoutOptions v-model="layout"
+                            <span class="ml-1 p-input-icon-left ">
+                                <i class="pi pi-search " style="color: blue;"/>
+                                <InputText 
+                                type="text" v-model="filteredText" 
+                                placeholder="Buscar" 
+                                style="width: 8em; color: blue;"/>
+                            </span>
+                        <DataViewLayoutOptions v-model="layout"
                           class="flex-1 flex justify-content-end font-bold border-round"/>
-                  </div>
-              </div>
-          </template>
-              <template #list="" v-for="index in filterCustomers" :key="index.id">
-                  <div class="col-12" >
-                      <div class="flex flex-column xl:flex-row xl:align-items-start p-4 gap-4" >
-                          <div class="flex flex-column sm:flex-row justify-content-between align-items-center xl:align-items-start flex-1 gap-4">
-                              <div class="flex flex-column align-items-center sm:align-items-start gap-3">
-                                  <img class="w-4 sm:w-5rem xl:w-2rem shadow-2 block xl:block mx-auto border-round" src="@/assets/img/_copy.jpg" alt="image client" />
-                                  <div class="text-2xl font-bold text-900">{{ index.cliente }}</div>
-                                  <span class="font-semibold">{{ index.address }}</span>
-                                  <span class="font-semibold">{{ index.dni }}</span>
-                                    <div class="flex align-items-center gap-3">
-                                      <span class="flex align-items-center gap-2">
-                                          <i class="pi pi-tag"></i>
-                                      </span>
-                                  </div>
-                              </div>
-                              <div class="flex sm:flex-column align-items-center sm:align-items-end gap-3 sm:gap-2">
-                                  <span class="text-2xl font-semibold">{{ index.phone }}</span>
-                                  <Tag severity="success" value="Success"></Tag>
-                                  <Button icon="pi pi-eye"
-                                          label="ver"
-                                          :loading="loading"
-                                          @click="onclicked(index.id)"/>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </template>
-              <template #grid="" v-for="index in filterCustomers" :key="index.id">
-                  <div class="col-12 sm:col-6 lg:col-12 xl:col-4 p-2" >
-                      <div class="p-4 border-1 surface-border surface-card border-round" >
-                          <div class="flex flex-wrap align-items-center justify-content-between gap-2">
-                          </div>
-                          <div class="flex flex-column align-items-center gap-3 py-5">
-                              <img class="w-4 sm:w-5rem xl:w-2rem shadow-2 block xl:block mx-auto border-round" src="@/assets/img/_copy.jpg" alt="image client" />
-                              <div class="text-2xl font-bold">{{ index.cliente }}</div>
-                              <span class="font-semibold">{{ index.address }}</span>
-                              <div class="flex align-items-center gap-2">
-                                  <span class="text-2xl font-semibold">{{ index.phone}}</span>
-                              </div>
-                              <div class="flex align-items-center justify-content-between">
-                                  <span class="font-semibold">{{ index.dni }} </span>
-                                  <i class="pi pi-tag"></i>
-                              </div>
-                              <div class="flex sm:flex-column align-items-center sm:align-items-start gap-3 sm:gap-2">
-                                  <Tag severity="success" value="Success"></Tag>
-                              </div>
-                              <div class="flex sm:flex-column align-items-center sm:align-items-end gap-3 sm:gap-2">
-                                  <Button icon="pi pi-eye"
-                                          label="ver"
-                                          :loading="loading"
-                                          @click="onclicked(index.id)"/>
-                              </div>
-                          </div>
-                          
-                      </div>
-                  </div>
-              </template>
+                    </div>
+                </div>
+            </template>
+            <template #list="" >
+                <div class="col-12" v-for="index in filterCustomers" :key="index.id">
+                    <div class="flex flex-column xl:flex-row xl:align-items-start p-4 gap-4" >
+                        <div class="flex flex-column sm:flex-row justify-content-between align-items-center xl:align-items-start flex-1 gap-4">
+                            <div class="flex flex-column align-items-center sm:align-items-start gap-3">
+                                <img class="w-4 sm:w-5rem xl:w-2rem shadow-2 block xl:block mx-auto border-round" src="@/assets/img/_copy.jpg" alt="image client" />
+                                <div class="text-2xl font-bold text-900">{{ index.cliente }}</div>
+                                <span class="font-semibold">{{ index.address }}</span>
+                                <span class="font-semibold">{{ index.dni }}</span>
+                                <div class="flex align-items-center gap-3">
+                                    <span class="flex align-items-center gap-2">
+                                        <i class="pi pi-tag"></i>
+                                    </span>
+                                </div>
+                            </div>
+                            <div class="flex sm:flex-column align-items-center sm:align-items-end gap-3 sm:gap-2">
+                                <span class="text-2xl font-semibold">{{ index.phone }}</span>
+                                <Tag severity="success" value="Success"></Tag>
+                                <Button icon="pi pi-eye"
+                                    label="ver"
+                                    :loading="loading"
+                                    @click="onclicked(index.id)"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </template>
+            <template #grid="">
+                <div class="grid grid-nogutter">
+                    <div v-for="index in filterCustomers" :key="index.id" class="col-12 sm:col-6 lg:col-12 xl:col-4 p-2">
+                        <div class="p-4 border-1 surface-border surface-card border-round">
+                            <div class="flex flex-wrap align-items-center justify-content-between gap-2">
+                            </div>
+                            <div class="flex flex-column align-items-center gap-3 py-5">
+                            <img class="w-4 sm:w-5rem xl:w-2rem shadow-2 block xl:block mx-auto border-round" src="@/assets/img/_copy.jpg" alt="image client" />
+                            <div class="text-2xl font-bold">{{ index.cliente }}</div>
+                            <span class="font-semibold">{{ index.address }}</span>
+                            <div class="flex align-items-center gap-2">
+                                <span class="text-2xl font-semibold">{{ index.phone}}</span>
+                            </div>
+                            <div class="flex align-items-center justify-content-between">
+                                <span class="font-semibold">{{ index.dni }} </span>
+                                <i class="pi pi-tag"></i>
+                            </div>
+                            <div class="flex sm:flex-column align-items-center sm:align-items-start gap-3 sm:gap-2">
+                                <Tag severity="success" value="Success"></Tag>
+                            </div>
+                            <div class="flex sm:flex-column align-items-center sm:align-items-end gap-3 sm:gap-2">
+                                <Button icon="pi pi-eye"
+                                        label="ver"
+                                        :loading="loading"
+                                        @click="onclicked(index.id)"/>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </template>
       </DataView>
   </div>
 </template>
